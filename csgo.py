389f42216b455e60d52e0983f34386e6d677a220
Maciej Majek <Maciek.Majek2@gmail.com>
2020-10-08 23:41:04 +0200

    Format and clean the code

--- csgo.py
+++ csgo.py
@@ -7,10 +7,12 @@ import numpy as np
 
 API_KEY = ""
 
+
 def search(stats, key):
     for stat in stats:
         if stat['name'] == key:
             return stat['value']
+
 
 class SteamProfile:
     def __init__(self, steamURL, profileID=""):
@@ -18,7 +20,7 @@ class SteamProfile:
         self.profileID = profileID
 
     steamID = ""
-    steamURL= ""
+    steamURL = ""
     profileID = ""
     csgoStats = {}
     cleanCsgoStats = {}
@@ -27,44 +29,47 @@ class SteamProfile:
     def __getProfileID(self):
         try:
             self.profileID = re.search(r'/id/(.*?)/', self.steamURL).group(1)
-            print(self.steamID)
         except AttributeError:
             self.profileID = None
 
     def __cleanCsgoStats(self):
         json_str = str(self.csgoStats)
-        json_str = re.search(r'\{(.*?)\[(.*?)\](.*?)\]\}', json_str.replace(' ','')).group(2).replace("'",'"')
+        json_str = re.search(
+            r'\{(.*?)\[(.*?)\](.*?)\]\}', json_str.replace(' ', '')).group(2).replace("'", '"')
         properties = re.findall(r'\{"name":"(.*?)","value":(.*?)\}', json_str)
-        
-        for p in properties:
-            self.cleanCsgoStats[p[0]]=p[1]
-        self.__getWeaponStats()
-    def __getWeaponStats(self):
-        weapons_shots = re.findall(r"total_shots_(.*?)\'",str(self.cleanCsgoStats))
-        weapons_hits  = re.findall(r"total_hits_(.*?)\'",str(self.cleanCsgoStats))
 
-        #self.weaponsStats['shot'] = self.cleanCsgoStats['total_shots_fired']
-        #self.weaponsStats['hit'] =self.cleanCsgoStats['total_shots_hit']
-        
+        for p in properties:
+            self.cleanCsgoStats[p[0]] = p[1]
+        self.__getWeaponStats()
+
+    def __getWeaponStats(self):
+        weapons_shots = re.findall(
+            r"total_shots_(.*?)\'", str(self.cleanCsgoStats))
+        weapons_hits = re.findall(
+            r"total_hits_(.*?)\'", str(self.cleanCsgoStats))
+
         for weapon in weapons_shots:
             if weapon != 'hit' and weapon != 'fired':
                 self.weaponsStats[weapon] = []
-                self.weaponsStats[weapon].append(int(self.cleanCsgoStats['total_shots_'+weapon]))
+                self.weaponsStats[weapon].append(
+                    int(self.cleanCsgoStats['total_shots_'+weapon]))
                 try:
-                    self.weaponsStats[weapon].append(int(self.cleanCsgoStats['total_hits_'+weapon]))
+                    self.weaponsStats[weapon].append(
+                        int(self.cleanCsgoStats['total_hits_'+weapon]))
                 except KeyError:
                     self.weaponsStats[weapon].append(0)
 
     def getSteamID(self):
         self.__getProfileID()
         if self.profileID:
-            request = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+API_KEY+'&vanityurl='+self.profileID)
+            request = requests.get(
+                'http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+API_KEY+'&vanityurl='+self.profileID)
             self.steamID = request.json()['response']['steamid']
-        print(self.steamID)
 
     def getCsgoStats(self):
         if self.profileID:
-            request = requests.get('http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key='+API_KEY+'&steamid='+self.steamID)
+            request = requests.get(
+                'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key='+API_KEY+'&steamid='+self.steamID)
             self.csgoStats = request.json()
             with open('csgo_stats.json', 'w') as stats_file:
                 json.dump(self.csgoStats, stats_file)
@@ -95,8 +100,8 @@ class SteamProfile:
         v1 = np.array(values)
         for i in range(len(v2)):
             v2[i] = [v2[i][0]-v2[i][1], v2[i][1]]
-        wedges, texts = ax.pie(v1[:,0], radius=1, colors=outer_colors,
-               wedgeprops=dict(width=size, edgecolor='w'))
+        wedges, texts = ax.pie(v1[:, 0], radius=1, colors=outer_colors,
+                               wedgeprops=dict(width=size, edgecolor='w'))
 
         ax.pie(v2.flatten(), radius=1-size, colors=inner_colors,
                wedgeprops=dict(width=size, edgecolor='w'))
@@ -108,7 +113,8 @@ class SteamProfile:
         v1 = np.array(values)
         recipe = []
         for i in range(len(list(keys))):
-            recipe.append(weapons[i]+" ("+str(v2.flatten()[2*i+1])+"/"+str(v2.flatten()[2*i])+")="+str(round(100*float(v2.flatten()[2*i+1])/float(v2.flatten()[2*i]),1))+"%")
+            recipe.append(weapons[i]+" ("+str(v2.flatten()[2*i+1])+"/"+str(v2.flatten()[2*i])+")="+str(
+                round(100*float(v2.flatten()[2*i+1])/float(v2.flatten()[2*i]), 1))+"%")
         for i, p in enumerate(wedges):
             ang = (p.theta2 - p.theta1)/2. + p.theta1
             y = np.sin(np.deg2rad(ang))
@@ -123,16 +129,23 @@ class SteamProfile:
 
     def __str__(self):
         json_str = str(self.csgoStats)
-        json_str = re.search(r'\{(.*?)\[(.*?)\](.*?)\]\}', json_str.replace(' ','')).group(2).replace("'",'"')
+        json_str = re.search(
+            r'\{(.*?)\[(.*?)\](.*?)\]\}', json_str.replace(' ', '')).group(2).replace("'", '"')
         properties = re.findall(r'\{"name":"(.*?)","value":(.*?)\}', json_str)
         data = {}
         for p in properties:
-            data[p[0]]=p[1]
+            data[p[0]] = p[1]
 
         line = "Total kills: {}\nTotal deaths: {}\nIngame time: {}h{}m\nOverall KD ratio: {}"
-        return line.format(data['total_kills'], data['total_deaths'], str(int(data['total_time_played'])//3600),str(int(data['total_time_played'])//60%60), float(data['total_kills'])/float(data['total_deaths']))
+        return line.format(data['total_kills'],
+                           data['total_deaths'],
+                           str(int(data['total_time_played'])//3600),
+                           str(int(data['total_time_played'])//60 % 60),
+                           float(data['total_kills'])/float(data['total_deaths']))
 
-profile = SteamProfile("https://steamcommunity.com/id/example/")
+
+profile = SteamProfile(
+    "https://steamcommunity.com/id/example/")
 
 profile.getSteamID()
 profile.getCsgoStats()
